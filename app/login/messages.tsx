03423c6ce186c1a044716172a15989382f3ae4d0
ido evergeen <dicksonevergreenido@gmail.com>
2023-11-22 12:14:58 +0100

    update middleware

--- app/login/messages.tsx
+++ app/login/messages.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-// import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 
 export default function Messages() {
-  // const searchParams = useSearchParams()
+  const searchParams = useSearchParams()
   const error = searchParams.get('error')
   const message = searchParams.get('message')
   return (
