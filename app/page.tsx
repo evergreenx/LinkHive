89a5077d29da0ee0ffd7047ea5dfbcbf893462c5
ido evergeen <dicksonevergreenido@gmail.com>
2023-12-08 05:15:09 +0100

    fix: resolve build error:

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,34 @@
 'use client';
 
 import * as React from 'react';
-import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+// import {  createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 export default function Page() {
-  const supabase = createClientComponentClient();
+  // const supabase = createClientComponentClient();
 
-  const [userData, setUserData] = React.useState<Session | null>(null);
+  // const [userData, setUserData] = React.useState<Session | null>(null);
 
-  React.useEffect(() => {
-    const handleFetchUser = async () => {
-      const {
-        data: { session }
-      } = await supabase.auth.getSession();
+  // React.useEffect(() => {
+  //   const handleFetchUser = async () => {
+  //     const {
+  //       data: { session }
+  //     } = await supabase.auth.getSession();
 
-      setUserData(session);
-    };
+  //     setUserData(session);
+  //   };
 
-    handleFetchUser();
-  }, [supabase.auth]);
+  //   handleFetchUser();
+  // }, [supabase.auth]);
 
-  const router = useRouter();
+  // const router = useRouter();
 
-  if (userData) {
-    return router.push('/dashboard/links');
-  } else {
-    return router.push('/login');
-  }
+  // if (userData) {
+  //   return router.push('/dashboard/links');
+  // } else {
+  //   return router.push('/login');
+  // }
 
   return <></>;
 }
